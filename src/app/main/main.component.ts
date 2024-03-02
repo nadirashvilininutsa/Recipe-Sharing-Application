790d3f3b7a50d34a716ef0d70578bfbb95c1f8b1
nadirashvilininutsa <160115012+nadirashvilininutsa@users.noreply.github.com>
2024-03-02 19:18:58 +0400

    5-search-functionality - Implement search functionality inside main component - User should be able to search for recipes based on title or ingredient

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,7 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonService } from '../services/common.service';
-import { Recipe } from '../models/recipe-models';
-import { Observable } from 'rxjs';
+import { Recipe, searchRecipe } from '../models/recipe-models';
+import {
+  Observable,
+  debounceTime,
+  distinctUntilChanged,
+  filter,
+  map,
+  of,
+  switchMap,
+  tap,
+} from 'rxjs';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-main',
@@ -9,9 +19,10 @@ import { Observable } from 'rxjs';
   styleUrls: ['./main.component.scss'],
 })
 export class MainComponent {
-  constructor(private commonService: CommonService) {}
+  constructor(private commonService: CommonService, private fb: FormBuilder) {}
 
-  recipes$: Observable<Recipe[]> | undefined;
+  recipes$: Observable<Recipe[]> = of();
+  filteredRecipes$: Observable<Recipe[]> = of();
   recipeDetails: Recipe | undefined;
 
   getRecipeList(): Observable<Recipe[]> {
@@ -26,7 +37,50 @@ export class MainComponent {
     this.recipeDetails = undefined;
   }
 
+  form: FormGroup = this.buildForm();
+
+  private buildForm() {
+    return this.fb.group<searchRecipe>({
+      title: this.fb.control(''),
+      ingredient: this.fb.control(''),
+    });
+  }
+
+  filterRecipe() {
+    const title: string = this.form.controls['title'].value.toLowerCase();
+    const product: string =
+      this.form.controls['ingredient'].value.toLowerCase();
+
+    if (!title && !product) {
+      this.filteredRecipes$ = this.recipes$;
+      return;
+    }
+
+    this.filteredRecipes$ = this.recipes$.pipe(
+      distinctUntilChanged(),
+      debounceTime(500),
+      map((recipes) =>
+        recipes.filter((recipe) => {
+          const matchTitle = recipe.title.toLowerCase().includes(title);
+          const matchProduct = recipe.ingredients.filter((ingredient) =>
+            ingredient.product.toLowerCase().includes(product)
+          );
+
+          if (title && product) {
+            return matchTitle && matchProduct.length > 0 ? true : false;
+          } else if (title && !product) {
+            return matchTitle;
+          } else if (product) {
+            return matchProduct.length > 0;
+          }
+          return false;
+        })
+      )
+    );
+  }
+
   ngOnInit() {
     this.recipes$ = this.getRecipeList();
+    this.filteredRecipes$ = this.recipes$;
   }
 }
